d65f89bf82b21b0a98e13ff1ab03c7374b85a37d
zjy <zjy@zjy.com>
2025-11-28 13:20:27 +0800

    初始

---
+++ src/test/java/com/zjy/shuati_ya/ShuatiYaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjy.shuati_ya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShuatiYaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
